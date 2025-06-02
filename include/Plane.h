6853f2c2c96367fe031e93e6093e9dab46514c66
Martin Hertel <martin.hertel01@gmail.com>
2025-06-02 23:45:23 +0200

    [Added] Plane class

--- include/Plane.h
+++ include/Plane.h
@@ -8,6 +8,41 @@
 
 namespace geometry {
 
+    class Plane {
+    public:
+        Plane() = default;
+
+        Plane(const Vector3D &normal, float d) : normal(normal), d(d) {}
+
+        Vector3D normal; // Normal vector of the plane
+        float d;         // Distance from the origin
+
+        // Function to get a string representation of the plane
+        std::string toString() const {
+            return "Plane(normal: " + normal.toString() + ", d: " + std::to_string(d) + ")";
+        }
+
+        // Function to check if a point is on the plane
+        bool contains(const Vector3D &point) const {
+            return normal.dot(point) + d == 0;
+        }
+
+        // Function to get the distance from a point to the plane
+        float distanceToPoint(const Vector3D &point) const {
+            return (normal.dot(point) + d) / normal.length();
+        }
+
+        // Function to get the intersection point of a line and the plane
+        Vector3D intersection(const Vector3D &linePoint, const Vector3D &lineDirection) const {
+            float t = -(normal.dot(linePoint) + d) / normal.dot(lineDirection);
+            return linePoint + lineDirection * t;
+        }
+
+        // Equality operator for Plane
+        bool operator==(const Plane &other) const {
+            return normal == other.normal && d == other.d;
+        }
+    };
 }
 
 #endif //GEOMETRY_PLANE_H
